14920bddcf61fa4d42ad03ab8e60d4fd27cb5ab1
dirigonaut <>
2018-04-22 13:52:33 -0700

    Added in schema validation.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # build-pulse-project
+
+git pull
+install python
+install pip
+cd into project_folder/build-pulse-project
+virtualenv build-pulse-project
+source build-pulse-project/bin/activate
+pip install requirements
+flask initdb
+flask run
--- build-pulse-project/schema.sql
+++ build-pulse-project/schema.sql
@@ -3,9 +3,9 @@ drop table if exists cars;
 create table cars (
   _id text primary key,
   make text not null,
-  year text not null,
+  year number not null,
   color text not null,
-  price text not null,
+  price number not null,
   hasSunroof boolean not null,
   isFourWheelDrive boolean not null,
   hasLowMiles boolean not null,
